438c128c11ae37bc001c65152be5b157b623b92a
AdityaK85 <adityakothekar79@gmail.com>
2024-03-03 18:38:45 +0530

    today

--- Python_website/settings.py
+++ Python_website/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -116,8 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+
+# Production Server
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = 'static/'
 STATIC_DIRS = (os.path.join(BASE_DIR,'/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'/static')
+
+
+# Local Server
+# STATIC_URL = 'static/'
+# STATIC_DIRS = (os.path.join(BASE_DIR,'/static'),)
 # STATIC_ROOT = os.path.join(BASE_DIR,'/static')
 
 
